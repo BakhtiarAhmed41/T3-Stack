ef7ea5ad1a31a1d5c5ef323044a52ba54f9d83e1
BakhtiarAhmed41 <ahmedbakhtiar41@gmail.com>
2024-01-05 16:33:01 +0500

    some changes

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,11 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import {z} from "zod";
-
+import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.post.findMany();
-}),
+  }),
 });
